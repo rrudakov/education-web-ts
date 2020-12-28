fed70716004ed6a1b995e3331817a0d11d908d6e
Roman Rudakov <rrudakov@protonmail.com>
2020-12-28 16:13:58 +0100

    Separate environments

--- src/core/constants.ts
+++ src/core/constants.ts
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://alenkinaskazka.net/api';
+export const BASE_URL = process.env.REACT_APP_BASE_URL;
 export const EMAIL_LINK = 'mailto:singerofnight@gmail.com';
 export const WHATSAPP_LINK = 'https://wa.me/31640718680';
 export const TELEGRAM_LINK = 'https://t.me/alionarudakova';
